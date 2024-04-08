a7efcf5e9e5ed8030391a561a929be55f0c8c6db
claudio <claudiomr79@gmail.com>
2024-04-08 16:33:33 -0300

    proyecto terminado

--- StockControl/compra/admin.py
+++ StockControl/compra/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Proveedor, Producto
-
-admin.site.register(Proveedor)
-admin.site.register(Producto)
+from .models import Producto, Proveedor
 
 
-# Register your models here.
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'precio', 'stock_actual']
+    search_fields = ['nombre', 'precio', 'stock_actual']
+admin.site.register(Producto,ProductoAdmin)
+
+
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido', 'dni']
+    search_fields = ['nombre', 'apellido', 'dni']
+admin.site.register(Proveedor,ProveedorAdmin)
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
-Para ingresar a Django admin, http://127.0.0.1:8000/admin se ingresa con Username: claudio y Password: admin
+# Sistema de gestión de personas
+
+## Trabajo integrador individual para Alkemy curso de POO en Python
+
+Para ingresar a Django admin, http://127.0.0.1:8000/admin se ingresa con:
+Username: claudio y Password: admin
