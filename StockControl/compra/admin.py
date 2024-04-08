ad68ae3b3b07944878194746590e581ac8f140b2
claudio <claudiomr79@gmail.com>
2024-04-08 11:15:32 -0300

    se agregaron los modelos, requirements.txt y readme

--- StockControl/compra/admin.py
+++ StockControl/compra/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Proveedor, Producto
+
+admin.site.register(Proveedor)
+admin.site.register(Producto)
+
 
 # Register your models here.
---
+++ readme.md
@@ -0,0 +1,1 @@
+Para ingresar a Django admin, http://127.0.0.1:8000/admin se ingresa con Username: claudio y Password: admin
